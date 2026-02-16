8595b0b633421de3fc2a74c1503a72637a125603
Natnael Ayele <n.eticha@alustudent.com>
2026-02-16 21:21:28 +0300

    changed duplicate word checking regex

--- scripts/validators.js
+++ scripts/validators.js
@@ -3,7 +3,8 @@ export const regex = {
     amount: /^(?:0\.(?:[1-9]\d?)|[1-9]\d*(?:\.\d{1,2})?)$/,
     date: /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$/,
     category: /^[A-Za-z]+(?:[ -][A-Za-z]+)*$/,
-    duplicateWords: /\b(\w+)\s+\1\b/i
+    duplicateWords: /\b(\w+)\b\s+\b\1\b/
+
 };
 
 
